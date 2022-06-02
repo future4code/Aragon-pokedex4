e6e6ff213c9fc360ee7951ec1302e20f018b2039
Larissa <larissadecastro18@gmail.com>
2022-06-02 20:29:43 -0300

    correção

--- pokedex/src/pages/PokedexPage.js
+++ pokedex/src/pages/PokedexPage.js
@@ -22,7 +22,7 @@ export default function PokedexPage() {
             <Header
                 actualPage={"pokedexpage"}
             />
-            
+           
             {showPokedex}
         </main>
 
